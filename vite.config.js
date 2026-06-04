e6c3351ca8ae951c1f27095555eb57d06e9b4e3a
ucfzem <azer.tyu199p@gmail.com>
2026-06-04 00:56:47 +0000

    fix base path to /nanogen/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/projets/',
+  base: '/nanogen/',
   server: {
     host: true,
     port: 3000
